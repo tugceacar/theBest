7d9af9a7f138406f3d656a6511b20790bc5fc5f1
emrahselek <emrahselek10@gmail.com>
2021-09-29 19:38:34 -0600

    new

---
+++ src/test/java/get_method/Get01_theBest.java
@@ -0,0 +1,32 @@
+package get_method;
+
+public class Get01_theBest {
+
+    /* Test Case:
+
+        Given
+            https://restful-booker.herokuapp.com/booking/20
+        When
+ 		    I send GET Request to the URL
+ 	    Then
+ 		    Status code is 200
+ 		And
+ 		    Response body is like
+              {
+                    "firstname": "Emrah",
+                    "lastname": "Selek",
+                    "totalprice": 3500,
+                    "depositpaid": true,
+                    "bookingdates": {
+                                "checkin": "2021-09-21",
+                                "checkout": "2021-09-25"
+                               },
+                    "additionalneeds": "Dinner"
+                }
+     */
+
+
+
+
+
+}
